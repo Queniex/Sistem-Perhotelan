dfe9a2c5eb2dce680dcc890bb7f6d515305b7635
Queniex <Queniesalbiyah@gmail.com>
2022-12-22 23:56:32 +0700

    Update form login

--- Perhotelan/Form/Login_Regist/Login.vb
+++ Perhotelan/Form/Login_Regist/Login.vb
@@ -1,3 +1,29 @@
-﻿Public Class Login
+﻿Imports System.Buffers
+Imports System.Security.Cryptography
+Public Class Login
+    Public Shared Users As Users
+    'Public Shared test As test
+    Private TripleDes As New TripleDESCryptoServiceProvider
 
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Users = New Users()
+    End Sub
+
+    Private Sub LblRegistrasi_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LblRegistrasi.LinkClicked
+        Dim reg As New Regist()
+        reg.Show()
+    End Sub
+
+    Private Sub ChkShow_CheckedChanged(sender As Object, e As EventArgs) Handles ChkShow.CheckedChanged
+        If ChkShow.Checked = True Then
+            TxtPassword.UseSystemPasswordChar = False
+        Else
+            TxtPassword.UseSystemPasswordChar = True
+        End If
+    End Sub
 End Class
