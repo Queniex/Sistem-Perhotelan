2eedcee2b1a9e72aba37a4bfe9b2e726049f297d
Queniex <Queniesalbiyah@gmail.com>
2022-12-22 15:25:21 +0700

    menambahkan form login dan regist

---
+++ Perhotelan/Login_Regist/Preview.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Preview
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Preview))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnX = New System.Windows.Forms.Label()
+        Me.BtnOk = New System.Windows.Forms.Button()
+        Me.PbUser = New System.Windows.Forms.PictureBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PbUser, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.BtnX)
+        Me.Panel1.Location = New System.Drawing.Point(0, 14)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(242, 50)
+        Me.Panel1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(4, 8)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(166, 31)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Image Preview"
+        '
+        'BtnX
+        '
+        Me.BtnX.AutoSize = True
+        Me.BtnX.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnX.ForeColor = System.Drawing.Color.White
+        Me.BtnX.Location = New System.Drawing.Point(200, 8)
+        Me.BtnX.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.BtnX.Name = "BtnX"
+        Me.BtnX.Size = New System.Drawing.Size(29, 31)
+        Me.BtnX.TabIndex = 0
+        Me.BtnX.Text = "X"
+        '
+        'BtnOk
+        '
+        Me.BtnOk.BackColor = System.Drawing.Color.Black
+        Me.BtnOk.ForeColor = System.Drawing.Color.White
+        Me.BtnOk.Location = New System.Drawing.Point(23, 330)
+        Me.BtnOk.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnOk.Name = "BtnOk"
+        Me.BtnOk.Size = New System.Drawing.Size(193, 38)
+        Me.BtnOk.TabIndex = 4
+        Me.BtnOk.Text = "OK"
+        Me.BtnOk.UseVisualStyleBackColor = False
+        '
+        'PbUser
+        '
+        Me.PbUser.BackColor = System.Drawing.Color.White
+        Me.PbUser.Location = New System.Drawing.Point(23, 95)
+        Me.PbUser.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PbUser.Name = "PbUser"
+        Me.PbUser.Size = New System.Drawing.Size(193, 225)
+        Me.PbUser.TabIndex = 3
+        Me.PbUser.TabStop = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(238, 75)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 106
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 392)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(238, 75)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 107
+        Me.PictureBox1.TabStop = False
+        '
+        'Preview
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.BtnOk)
+        Me.Controls.Add(Me.PbUser)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Preview"
+        Me.Size = New System.Drawing.Size(238, 428)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PbUser, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnX As Label
+    Friend WithEvents BtnOk As Button
+    Friend WithEvents PbUser As PictureBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
