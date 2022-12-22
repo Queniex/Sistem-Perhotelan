bfe10aacd6efa8a8557cd6b1b6223552d21032ba
Queniex <Queniesalbiyah@gmail.com>
2022-12-23 00:59:40 +0700

    Menambahkan tombol log out

--- Perhotelan/Form/Penginapan/Penginapan.vb
+++ Perhotelan/Form/Penginapan/Penginapan.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Penginapan
-
+    Private Sub BtnLogout_Click(sender As Object, e As EventArgs) Handles BtnLogout.Click
+        Me.Close()
+        Login.Show()
+    End Sub
 End Class
