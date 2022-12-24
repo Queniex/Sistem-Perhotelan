05751f699fcc14981767ba87a23c7a6f88d63d2a
dinocoww <nabiilah.afiifah26@gmail.com>
2022-12-25 01:25:21 +0700

    nambah code form tambah kamar

--- Perhotelan/Form/JenisKamar/TambahJenisKamar.vb
+++ Perhotelan/Form/JenisKamar/TambahJenisKamar.vb
@@ -1,3 +1,44 @@
 ﻿Public Class TambahJenisKamar
+    Public Sub New()
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub TxtBoxJKamar_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtBoxJKamar.KeyPress
+        If TxtBoxJKamar.Text.Length < 1 Then
+            TxtBoxJKamar.Select()
+            MessageBox.Show("Please add At least 1 String")
+        End If
+    End Sub
+
+    Private Sub TxtBoxHarga_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtBoxHarga.KeyPress
+        If Not Char.IsNumber(e.KeyChar) And Not e.KeyChar = Chr(Keys.Delete) And Not e.KeyChar = Chr(Keys.Back) Then
+            e.Handled = True
+            MessageBox.Show("Hanya angka yang dapat di input!")
+        End If
+
+        If TxtBoxJKamar.Text.Length < 1 Then
+            TxtBoxJKamar.Select()
+            MessageBox.Show("Please add At least 1 String")
+        End If
+    End Sub
+
+    Private Sub BtnBatalKamar_Click(sender As Object, e As EventArgs) Handles BtnBatalKamar.Click
+        JenisKamar_.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub BtnSimpan_Click(sender As Object, e As EventArgs) Handles BtnSimpan.Click
+        JenisKamar_.JenisKamar.GSJenis = TxtBoxJKamar.Text
+        JenisKamar_.JenisKamar.GSHarga = Integer.Parse(TxtBoxHarga.Text)
+
+        Dim info = New InfoJenisKamar()
+        info.Show()
+
+        Me.Close()
+    End Sub
 End Class
