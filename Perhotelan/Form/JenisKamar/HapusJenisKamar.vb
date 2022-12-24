b2fee25cef18741c693536795c5c4401a1c8a0d6
dinocoww <nabiilah.afiifah26@gmail.com>
2022-12-25 01:28:47 +0700

    nambah code form hapus kamar

--- Perhotelan/Form/JenisKamar/HapusJenisKamar.vb
+++ Perhotelan/Form/JenisKamar/HapusJenisKamar.vb
@@ -1,3 +1,9 @@
 ﻿Public Class HapusJenisKamar
+    Private Sub BtnBatal_Click(sender As Object, e As EventArgs) Handles BtnBatal.Click
 
+    End Sub
+
+    Private Sub BtnHapusJKamar_Click(sender As Object, e As EventArgs) Handles BtnHapusJKamar.Click
+
+    End Sub
 End Class
