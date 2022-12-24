f4aef951d0b3aa12bb9a1f7e25bb62f0c52da7fe
dinocoww <nabiilah.afiifah26@gmail.com>
2022-12-25 01:30:42 +0700

    nambah code form edit kamar

--- Perhotelan/Form/JenisKamar/EditJenisKamar.vb
+++ Perhotelan/Form/JenisKamar/EditJenisKamar.vb
@@ -1,3 +1,17 @@
 ﻿Public Class EditJenisKamar
+    Private Sub TxtBoxJKamar_TextChanged(sender As Object, e As EventArgs) Handles TxtBoxJKamar.TextChanged
 
+    End Sub
+
+    Private Sub TxtBoxHarga_TextChanged(sender As Object, e As EventArgs) Handles TxtBoxHarga.TextChanged
+
+    End Sub
+
+    Private Sub BtnBatalKamar_Click(sender As Object, e As EventArgs) Handles BtnBatalKamar.Click
+
+    End Sub
+
+    Private Sub BtnEditJKamar_Click(sender As Object, e As EventArgs) Handles BtnEditJKamar.Click
+
+    End Sub
 End Class
