9615ae92fdc27351387e8e7cead6f0da0b6de65a
dinocoww <nabiilah.afiifah26@gmail.com>
2022-12-26 20:35:03 +0700

    nambah info jenis kamar

--- Perhotelan/Form/JenisKamar/InfoJenisKamar.vb
+++ Perhotelan/Form/JenisKamar/InfoJenisKamar.vb
@@ -1,5 +1,16 @@
 ﻿Public Class InfoJenisKamar
-    Private Sub BtnKembali_Click(sender As Object, e As EventArgs) Handles BtnKembali.Click
+    Public Sub New()
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        LblJenisKamar.Text = JenisKamar_.JenisKamar.GSJenis
+        LblHarga.Text = JenisKamar_.JenisKamar.GSHarga
+    End Sub
+
+    Private Sub BtnKembali_Click(sender As Object, e As EventArgs) Handles BtnKembali.Click
+        JenisKamar_.Show()
+        Me.Close()
     End Sub
 End Class
