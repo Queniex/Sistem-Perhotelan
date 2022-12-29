0f8112eb26292991d95632245a3e80bc4d759b41
Laras Maharani <larasmharani@gmail.com>
2022-12-29 21:32:57 +0700

    Class Kamar and Connect DB

--- Perhotelan/Form/Kamar/Kamar.vb
+++ Perhotelan/Form/Kamar/Kamar.vb
@@ -1,10 +1,24 @@
-﻿Public Class Kamar
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+
+Public Class Kamar
+    Public Shared Class_Kamar As Class_Kamar
+
     Public Sub New(ByVal Username As String, ByVal Email As String)
 
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
+        Class_Kamar = New Class_Kamar()
+        ReloadDataTableDatabase()
+    End Sub
 
+    Private Sub ReloadDataTableDatabase()
+        DataGridViewKamar.DataSource = Class_Kamar.GetDataKoleksiDatabase()
+    End Sub
+
+    Private Sub Kamar_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        ReloadDataTableDatabase()
     End Sub
 End Class
