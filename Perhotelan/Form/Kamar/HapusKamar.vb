1e6f9630f8647c6c5085ca16a79af3060081e787
Laras Maharani <larasmharani@gmail.com>
2022-12-31 13:24:46 +0700

    Add Button Form Hapus Kamar

--- Perhotelan/Form/Kamar/HapusKamar.vb
+++ Perhotelan/Form/Kamar/HapusKamar.vb
@@ -7,4 +7,10 @@
         ' Add any initialization after the InitializeComponent() call.
 
     End Sub
+
+    Private Sub BtnBatal_Click(sender As Object, e As EventArgs) Handles BtnBatal.Click
+        Dim k As New Kamar(Login.Users.GSUserName, Login.Users.GSEmail)
+        k.Show()
+        Me.Close()
+    End Sub
 End Class
