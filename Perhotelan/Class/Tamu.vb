c346380e34554be118d036f67dc4c198a8c70506
adeliactra <adelia021203@gmail.com>
2023-01-03 22:34:32 +0700

    tambah attribute

--- Perhotelan/Class/Tamu.vb
+++ Perhotelan/Class/Tamu.vb
@@ -1,3 +1,16 @@
-﻿Public Class Tamu
+﻿Imports System.Security.Cryptography
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+Imports MySql.Data.MySqlClient
+Imports Mysqlx.XDevAPI.Common
+Public Class Tamu
+    Private NIK As Integer
+    Private Nama As String
+    Private Alamat As String
+    Private Jenis_Kelamin As String
 
+    Public Shared dbConn As New MySqlConnection
+    Public Shared sqlCommand As New MySqlCommand
+    Public Shared sqlRead As MySqlDataReader
+    Private sqlQuery As String
 End Class
