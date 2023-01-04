5cacb73d767d685913e0b447dc24f2f1b611476b
adeliactra <adelia021203@gmail.com>
2023-01-04 09:54:03 +0700

    update form edit tamu

--- Perhotelan/Form/Tamu/EditTamu.vb
+++ Perhotelan/Form/Tamu/EditTamu.vb
@@ -16,4 +16,30 @@ Public Class EditTamu
             RdBtnWanita.Checked = True
         End If
     End Sub
+
+    Private Sub BtnBatal_Click(sender As Object, e As EventArgs) Handles BtnBatal.Click
+        Dim t As New Tamu_(Login.Users.GSUserName, Login.Users.GSEmail)
+        t.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub BtnEditTamu_Click(sender As Object, e As EventArgs) Handles BtnEditTamu.Click
+        Tamu_.tamu.GSNIK = Integer.Parse(TxtNIK.Text)
+        Tamu_.tamu.GSNama = TxtNamaTamu.Text.ToString()
+        Tamu_.tamu.GSAlamat = RtbAlamat.Text.ToString()
+
+        If RdBtnPria.Checked Then
+            Tamu_.tamu.GSJenisKelamin = "Pria"
+        ElseIf RdBtnWanita.Checked Then
+            Tamu_.tamu.GSJenisKelamin = "Wanita"
+        End If
+
+        Tamu_.tamu.UpdateDataTamuByNIK(Tamu_.tamu.GSNIK,
+                                       Tamu_.tamu.GSNama,
+                                       Tamu_.tamu.GSAlamat,
+                                       Tamu_.tamu.GSJenisKelamin)
+        Dim Info = New InfoTamu()
+        Info.Show()
+        Me.Close()
+    End Sub
 End Class
