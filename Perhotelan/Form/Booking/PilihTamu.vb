3e36543e03d788d894c56dd89f255054d41f2136
adeliactra <adelia021203@gmail.com>
2023-01-05 14:50:55 +0700

    update form pilih tamu

--- Perhotelan/Form/Booking/PilihTamu.vb
+++ Perhotelan/Form/Booking/PilihTamu.vb
@@ -26,4 +26,22 @@
         selectedTableTamuNik = selectedRow.Cells(0).Value
         selectedTableTamuNama = selectedRow.Cells(1).Value
     End Sub
+
+    Private Sub BtnBatalDataTamu_Click(sender As Object, e As EventArgs) Handles BtnBatalDataTamu.Click
+        Dim bk As New BookingKamar_(Login.Users.GSUserName, Login.Users.GSEmail)
+        bk.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub BtnPilihDataTamu_Click(sender As Object, e As EventArgs) Handles BtnPilihDataTamu.Click
+        Try
+            BookingKamar_.textnamatamu = selectedTableTamuNama
+
+            Me.Close()
+            Dim bk As New BookingKamar_(Login.Users.GSUserName, Login.Users.GSEmail)
+            bk.Show()
+        Catch ex As Exception
+            MsgBox("Anda belum memilih baris apa pun!")
+        End Try
+    End Sub
 End Class
