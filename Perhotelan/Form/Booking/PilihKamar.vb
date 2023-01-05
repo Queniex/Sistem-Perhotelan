577ce73daac1f3f6b1d7120938e777c8ffc04274
adeliactra <adelia021203@gmail.com>
2023-01-05 14:46:33 +0700

    update form pilih kamar

--- Perhotelan/Form/Booking/PilihKamar.vb
+++ Perhotelan/Form/Booking/PilihKamar.vb
@@ -1,3 +1,24 @@
 ﻿Public Class PilihKamar
+    Public Shared selectedTableKamarID
+    Public Shared selectedTableKamarHarga
+    Public Shared selectedTableKamarNama
+    Public Shared selectedTableKamarJenis
+    Public Shared harga As Integer
 
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        ReloadDataTableDatabase()
+    End Sub
+
+    Private Sub ReloadDataTableDatabase()
+        DataGridViewDataKamar.DataSource = BookingKamar_.booking.getDataKamar()
+    End Sub
+
+    Private Sub PilihKamar_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        ReloadDataTableDatabase()
+    End Sub
 End Class
