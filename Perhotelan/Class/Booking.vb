797d3442b1add7f2ef7442354f3b6fbb675d25bc
adeliactra <adelia021203@gmail.com>
2023-01-05 14:10:55 +0700

    update class booking

--- Perhotelan/Class/Booking.vb
+++ Perhotelan/Class/Booking.vb
@@ -17,4 +17,40 @@ Public Class Booking
     Public Shared sqlCommand As New MySqlCommand
     Public Shared sqlRead As MySqlDataReader
     Private sqlQuery As String
+
+    Public Property GSIdTamu() As Integer
+        Get
+            Return id_tamu
+        End Get
+        Set(value As Integer)
+            id_tamu = value
+        End Set
+    End Property
+
+    Public Property GSIdKamar() As Integer
+        Get
+            Return id_kamar
+        End Get
+        Set(value As Integer)
+            id_kamar = value
+        End Set
+    End Property
+
+    Public Property GSTotal() As String
+        Get
+            Return totalHarga
+        End Get
+        Set(value As String)
+            totalHarga = value
+        End Set
+    End Property
+
+    Public Property GSNoKamar() As String
+        Get
+            Return NoKamar
+        End Get
+        Set(ByVal value As String)
+            NoKamar = value
+        End Set
+    End Property
 End Class
