04e8d32346967708b183a7738925944c7dfe1d8d
adeliactra <adelia021203@gmail.com>
2023-01-05 12:53:58 +0700

    update class booking

--- Perhotelan/Class/Booking.vb
+++ Perhotelan/Class/Booking.vb
@@ -1,3 +1,20 @@
-﻿Public Class Booking
+﻿Imports System.Security.Cryptography
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+Imports MySql.Data.MySqlClient
+Imports Mysqlx.XDevAPI.Common
+Public Class Booking
+    Private id_tamu As Integer
+    Private id_kamar As Integer
+    Private check_in As String
+    Private check_out As String
+    Private status As String
+    Private NoKamar As String
+    Private NamaTamu As String
+    Private totalHarga As String
 
+    Public Shared dbConn As New MySqlConnection
+    Public Shared sqlCommand As New MySqlCommand
+    Public Shared sqlRead As MySqlDataReader
+    Private sqlQuery As String
 End Class
