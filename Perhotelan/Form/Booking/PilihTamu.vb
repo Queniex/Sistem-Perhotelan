8182774a467ec8af4e18df7d1bb76ce03663511c
adeliactra <adelia021203@gmail.com>
2023-01-05 14:49:36 +0700

    update form pilih tamu

--- Perhotelan/Form/Booking/PilihTamu.vb
+++ Perhotelan/Form/Booking/PilihTamu.vb
@@ -1,3 +1,20 @@
 ﻿Public Class PilihTamu
+    Public Shared selectedTableTamuNik
+    Public Shared selectedTableTamuNama
+    Public Sub New()
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        ReloadDataTableDatabase()
+    End Sub
+
+    Private Sub ReloadDataTableDatabase()
+        DataGridViewDataTamu.DataSource = BookingKamar_.booking.getDataTamu()
+    End Sub
+
+    Private Sub PilihTamu_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        ReloadDataTableDatabase()
+    End Sub
 End Class
