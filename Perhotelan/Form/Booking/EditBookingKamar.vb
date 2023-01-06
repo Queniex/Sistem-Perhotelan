ec416c9d6298cd60b67f56288dce5f3637c7b154
adeliactra <adelia021203@gmail.com>
2023-01-06 09:20:08 +0700

    update form edit booking

--- Perhotelan/Form/Booking/EditBookingKamar.vb
+++ Perhotelan/Form/Booking/EditBookingKamar.vb
@@ -1,3 +1,16 @@
-﻿Public Class EditBookingKamar
+﻿Imports MySql.Data.MySqlClient
+Public Class EditBookingKamar
+    Public Sub New()
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        CbBooking.Text = BookingKamar_.selectedTableBookingNoKamar.ToString()
+        CbTamu.Text = BookingKamar_.selectedTableBookingNamaTamu.ToString()
+        DtpCheckIn.Value = BookingKamar_.booking.GSCheckIn.ToString()
+        DtpCheckOut.Value = BookingKamar_.booking.GSCheckOut.ToString()
+        CbStatusKamar.Text = BookingKamar_.booking.GSStatus.ToString()
+        LblTotalBayar.Text = BookingKamar_.booking.GSTotal.ToString()
+    End Sub
 End Class
