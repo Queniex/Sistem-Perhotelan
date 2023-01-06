6dc7154dc1019cdc1af355c1eb2f35c566923927
adeliactra <adelia021203@gmail.com>
2023-01-06 09:04:31 +0700

    update form hapus booking

--- Perhotelan/Form/Booking/HapusBookingKamar.vb
+++ Perhotelan/Form/Booking/HapusBookingKamar.vb
@@ -1,3 +1,15 @@
 ﻿Public Class HapusBookingKamar
+    Public Sub New()
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        LblNoBooking.Text = BookingKamar_.selectedTableBookingIdBooking.ToString()
+        LblNamaTamu.Text = BookingKamar_.selectedTableBookingNamaTamu.ToString()
+        LblCekin.Text = BookingKamar_.booking.GSCheckIn.ToString()
+        Lblcekout.Text = BookingKamar_.booking.GSCheckOut.ToString()
+        LblStatusKamar.Text = BookingKamar_.booking.GSStatus.ToString()
+        LblTotalBiaya.Text = BookingKamar_.booking.GSTotal.ToString()
+    End Sub
 End Class
