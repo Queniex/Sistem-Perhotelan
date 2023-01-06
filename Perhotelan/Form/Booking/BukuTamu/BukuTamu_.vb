a3abb3dc5ca32a6c36a911cc2b5e305c10ba9c19
Queniex <Queniesalbiyah@gmail.com>
2023-01-06 12:24:34 +0700

    Menambahkan constructor pada form BukuTamu

--- Perhotelan/Form/Booking/BukuTamu/BukuTamu_.vb
+++ Perhotelan/Form/Booking/BukuTamu/BukuTamu_.vb
@@ -1,5 +1,28 @@
 ﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
-
 Public Class BukuTamu_
+    Public Shared hargaTotal
+    Public Sub New(ByVal Username As String, ByVal Email As String)
 
+        ' This call is required by the designer.
+        InitializeComponent()
+        ReloadDataTableDatabase()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Dim usn As String = Username
+        LblUser.Text = usn.ToUpper()
+        LblEmail.Text = Email
+
+        If Not String.IsNullOrEmpty(Login.Users.GSFoto) Then
+            PictureBoxBukuTamu.Load(Login.Users.GSFoto)
+            PictureBoxBukuTamu.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+    End Sub
+
+    Private Sub ReloadDataTableDatabase()
+        DataGridViewCheckin.DataSource = BookingKamar_.booking.GetDataCheckinDatabase()
+    End Sub
+
+    Private Sub BukuTamu__Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        ReloadDataTableDatabase()
+    End Sub
 End Class
