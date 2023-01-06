4c11f078b6403228d9c5ec05108f88647a8625dc
Queniex <Queniesalbiyah@gmail.com>
2023-01-06 12:33:21 +0700

    Menambahkan fungsi pada setiap tombol pada form hapusbukutamu

--- Perhotelan/Form/Booking/BukuTamu/HapusBukuTamu.vb
+++ Perhotelan/Form/Booking/BukuTamu/HapusBukuTamu.vb
@@ -12,4 +12,30 @@
         LblStatusKamar.Text = BookingKamar_.booking.GSStatus.ToString()
         LblTotalBiaya.Text = BukuTamu_.hargaTotal.ToString()
     End Sub
+
+    Private Sub BtnBatal_Click(sender As Object, e As EventArgs) Handles BtnBatal.Click
+        BookingKamar_.selectedTableBookingNoKamar = ""
+        BookingKamar_.selectedTableBookingNamaTamu = ""
+        BookingKamar_.textnamatamu = ""
+        BookingKamar_.textnokamar = ""
+
+        Dim bk As New BukuTamu_(Login.Users.GSUserName, Login.Users.GSEmail)
+        bk.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub BtnHapusBukuTamu_Click(sender As Object, e As EventArgs) Handles BtnHapusBukuTamu.Click
+        BookingKamar_.booking.DeleteDataBookingById(BookingKamar_.selectedTableBookingIdBooking)
+        Dim status As String = "Belum Terisi"
+        BookingKamar_.booking.UpdateDataStatusKamarById(BookingKamar_.booking.GSIdKamar, status)
+
+        Dim bk As New BukuTamu_(Login.Users.GSUserName, Login.Users.GSEmail)
+        bk.Show()
+        Me.Close()
+
+        BookingKamar_.selectedTableBookingNoKamar = ""
+        BookingKamar_.selectedTableBookingNamaTamu = ""
+        BookingKamar_.textnamatamu = ""
+        BookingKamar_.textnokamar = ""
+    End Sub
 End Class
