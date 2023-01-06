1fae29d39246ede023542d265be7d022cd48069c
adeliactra <adelia021203@gmail.com>
2023-01-06 09:00:25 +0700

    update form info booking

--- Perhotelan/Form/Booking/InfoBookingKamar.vb
+++ Perhotelan/Form/Booking/InfoBookingKamar.vb
@@ -12,4 +12,15 @@
         LblStatusKamar.Text = BookingKamar_.booking.GSStatus.ToString()
         LblTotalBiaya.Text = BookingKamar_.booking.GSTotal.ToString()
     End Sub
+
+    Private Sub BtnKembali_Click(sender As Object, e As EventArgs) Handles BtnKembali.Click
+        BookingKamar_.selectedTableBookingNoKamar = ""
+        BookingKamar_.selectedTableBookingNamaTamu = ""
+        BookingKamar_.textnokamar = ""
+        BookingKamar_.textnamatamu = ""
+
+        Dim bk As New BookingKamar_(Login.Users.GSUserName, Login.Users.GSEmail)
+        bk.Show()
+        Me.Close()
+    End Sub
 End Class
